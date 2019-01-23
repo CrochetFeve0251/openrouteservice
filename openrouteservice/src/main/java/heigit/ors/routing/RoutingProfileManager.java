d7887276d8bee60c338175bb74db6d58cbd20aa0
takb <takara@openrouteservice.org>
2019-01-23 12:15:51 +0100

    added search option alternative_routes, added RouteResult processing for GHResponse with multiple paths, minor refactoring in RouteResultBuilder and  RoutingRequestProcessor

--- openrouteservice/src/main/java/heigit/ors/routing/RoutingProfileManager.java
+++ openrouteservice/src/main/java/heigit/ors/routing/RoutingProfileManager.java
@@ -346,7 +346,7 @@ public class RoutingProfileManager {
         return null;
     }
 
-    public RouteResult computeRoute(RoutingRequest req) throws Exception {
+    public RouteResult[] computeRoute(RoutingRequest req) throws Exception {
         List<GHResponse> routes = new ArrayList<GHResponse>();
 
 //System.out.println("PATCHED!!!!");
@@ -436,7 +436,7 @@ public class RoutingProfileManager {
             c0 = c1;
         }
 
-        return new RouteResultBuilder().createRouteResult(routes, req, (pathProcessor != null && (pathProcessor instanceof ExtraInfoProcessor)) ? ((ExtraInfoProcessor) pathProcessor).getExtras() : null);
+        return new RouteResultBuilder().createRouteResults(routes, req, (pathProcessor != null && (pathProcessor instanceof ExtraInfoProcessor)) ? ((ExtraInfoProcessor) pathProcessor).getExtras() : null);
     }
 
     private double getHeadingDirection(GHResponse resp) {
